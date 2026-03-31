1210f303eb58aa76d61349e1c5461b0578498608
pvslvch <polina.vasilevichh@gmail.com>
2026-03-31 15:14:13 +0200

    feat: initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,11 +6,11 @@ import { QueryProvider } from "@/shared/ui/query-provider";
 
 const notoSerifHeading = Noto_Serif({subsets:['latin'],variable:'--font-heading'});
 
-const publicSans = Public_Sans({subsets:['latin'],variable:'--font-sans'});
+const publicSans = Public_Sans({ subsets: ["latin"], variable: "--font-sans" });
 
 const geistMono = Geist_Mono({ subsets: ["latin"], variable: "--font-mono" });
 
-const lora = Lora({ subsets: ["latin"], variable: "--font-serif" });
+const lora = Lora({subsets:['latin'],variable:'--font-serif'});
 
 export const metadata: Metadata = {
   title: "What can I read today?",
@@ -28,9 +28,9 @@ export default function RootLayout({
       className={cn(
               "h-full",
               "antialiased",
-              lora.variable,
               geistMono.variable,
-            , "font-sans", publicSans.variable, notoSerifHeading.variable)}
+              publicSans.variable,
+            , "font-serif", lora.variable, notoSerifHeading.variable)}
     >
       <body className="min-h-full flex flex-col" suppressHydrationWarning>
         <QueryProvider>{children}</QueryProvider>
