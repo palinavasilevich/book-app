558f42fa9742e540eba54238f392d0beb2e3feed
pvslvch <polina.vasilevichh@gmail.com>
2026-03-31 15:54:23 +0200

    refactor(layout): change favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Lora, Geist_Mono, Public_Sans, Noto_Serif, IBM_Plex_Sans } from "next/font/google";
+import { Lora, Geist_Mono, Noto_Serif, IBM_Plex_Sans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/shared/lib/utils";
 import { Providers } from "@/shared/ui/providers";
@@ -9,15 +9,26 @@ const notoSerifHeading = Noto_Serif({
   variable: "--font-heading",
 });
 
-const ibmPlexSans = IBM_Plex_Sans({subsets:['latin'],variable:'--font-sans'});
+const ibmPlexSans = IBM_Plex_Sans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 const geistMono = Geist_Mono({ subsets: ["latin"], variable: "--font-mono" });
 
 const lora = Lora({ subsets: ["latin"], variable: "--font-serif" });
 
 export const metadata: Metadata = {
-  title: "What can I read today?",
-  description: "What can I read today?",
+  title: {
+    default: "What can I read today?",
+    template: "%s | What can I read today?",
+  },
+  description:
+    "Discover your next book to read with personalized recommendations.",
+
+  icons: {
+    icon: "/book.svg",
+  },
 };
 
 export default function RootLayout({
@@ -30,12 +41,14 @@ export default function RootLayout({
       lang="en"
       suppressHydrationWarning
       className={cn(
-              "h-full",
-              "antialiased",
-              lora.variable,
-              geistMono.variable,
-              notoSerifHeading.variable,
-            , "font-sans", ibmPlexSans.variable)}
+        "h-full",
+        "antialiased",
+        lora.variable,
+        geistMono.variable,
+        notoSerifHeading.variable,
+        "font-sans",
+        ibmPlexSans.variable,
+      )}
     >
       <body className="min-h-full flex flex-col" suppressHydrationWarning>
         <Providers>{children}</Providers>
